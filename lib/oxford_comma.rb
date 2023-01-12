62f2c7f01f5dc9b84a1bc049747166d29f8018bd
tomer <tomertal52@yahoo.com>
2023-01-12 14:25:22 -0500

    commit tree d805639c5c1b97acd44d7fdd83e1f00a9ce63c2a

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    if array.size == 1 
+        array.join
+    elsif array.size == 2
+        array.join(" and ")
+    elsif array.size > 2
+        array[-1] = "and " + array[-1];
+        array.join(", ")
+    end
 end
+
+puts oxford_comma(["kiwi"])
